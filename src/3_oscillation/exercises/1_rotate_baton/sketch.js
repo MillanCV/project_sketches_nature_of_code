794c7a37e8f12f2f1ea3961c614e252cc85db812
MillanCV <m.castrovilarino@gmail.com>
2024-10-06 09:33:21 +0200

    adapt canvas

--- src/3_oscillation/exercises/1_rotate_baton/sketch.js
+++ src/3_oscillation/exercises/1_rotate_baton/sketch.js
@@ -3,12 +3,12 @@
 let baton;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
   baton = new Baton();
 }
 
 function draw() {
   background(220);
 
-  baton.draw()
+  baton.draw();
 }
